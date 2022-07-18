1b404cca1dfbf488b25b9ede69e5c6aed082dec1
EnderTheCoder <ggameinvader@gmail.com>
2022-07-18 22:15:34 +0800

    Initial commit

---
+++ src/main/java/dev/ender/stardust/Stardust.java
@@ -0,0 +1,10 @@
+package dev.ender.stardust;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Stardust implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/ender/stardust/client/StardustClient.java
@@ -0,0 +1,13 @@
+package dev.ender.stardust.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class StardustClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
